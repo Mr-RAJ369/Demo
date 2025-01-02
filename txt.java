48f1748c230e22af79076c1db897cd8950c5940d
Mr-RAJ369 <rajkumar9631712149@gmail.com>
2025-01-02 18:24:16 +0530

    Print 1 to 10

--- txt.java
+++ txt.java
@@ -1,2 +1,8 @@
-ytcjyvdyyr
-  gvhjh
+import java.util.Scanner;
+class txt{
+    public static void main(String arg[]){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
